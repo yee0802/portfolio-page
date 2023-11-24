1e719cbd5f013f00376d8596adedadad697b335c
Kye <NovusAa12@proton.me>
2023-11-24 16:20:37 +0000

    add github logo link

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,11 @@ export default function Home() {
     <article>
       <h1>Hi, I'm Kye. 👋</h1>
       <p>An aspiring Full-Stack Developer. 👾</p>
+      <section>
+        <a href="https://github.com/yee0802" target="_blank">
+          <img src="../../src/assets/github.svg" alt="github-logo" width={23} />
+        </a>
+      </section>
     </article>
   );
 }
